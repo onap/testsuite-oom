431689c7879a92be54477f13f8e39908db5f07f2
Brian Freeman <bf1936@att.com>
2020-04-15 12:52:41 -0500

    Handle less than /16 oam network
    Matching changes in testsuite and demo
    
    Issue-ID: INT-1527
    Change-Id: Iac7cdb735c4544e0d6bc79c444ba9de32b79fc32
    Signed-off-by: Brian Freeman <bf1936@att.com>

--- resources/config/eteshare/config/robot_properties.py
+++ resources/config/eteshare/config/robot_properties.py
@@ -74,6 +74,7 @@ GLOBAL_INJECTED_OPENSTACK_TENANT_ID_REGION_THREE = '{{ .Values.openStackTenantId
 GLOBAL_INJECTED_OPENSTACK_PROJECT_DOMAIN_REGION_THREE = '{{ .Values.openStackProjectNameRegionThree }}'
 GLOBAL_INJECTED_OPENSTACK_USER_DOMAIN_REGION_THREE = '{{ .Values.openStackDomainIdRegionThree }}'
 GLOBAL_INJECTED_OPENSTACK_OAM_NETWORK_CIDR_PREFIX = '{{ .Values.openStackOamNetworkCidrPrefix }}'
+GLOBAL_INJECTED_OPENSTACK_OAM_NETWORK_3RD_OCTET = '{{ .Values.openStackOamNetwork3rdOctet}}'
 GLOBAL_INJECTED_OPENSTACK_PUBLIC_NETWORK = '{{ .Values.openStackPublicNetworkName }}'
 GLOBAL_INJECTED_OPENSTACK_SECURITY_GROUP = '{{ .Values.openStackSecurityGroup }}'
 GLOBAL_INJECTED_OPENSTACK_PRIVATE_SUBNET_ID = "{{ .Values.openStackPrivateSubnetId }}"
